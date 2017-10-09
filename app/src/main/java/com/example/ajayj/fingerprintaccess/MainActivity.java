68a0814240a56af6856268e9c532b41a90d1e553
Ajay Jain <ajayjain9832@gmail.com>
2017-10-09 23:23:15 +0530

    UI and design Changed

--- app/src/main/java/com/example/ajayj/fingerprintaccess/MainActivity.java
+++ app/src/main/java/com/example/ajayj/fingerprintaccess/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ajayj.fingerprintaccess;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,19 +10,37 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button b1;
+    Button b1,fbutton1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         b1=(Button) findViewById(R.id.abc);
+        fbutton1=(Button) findViewById(R.id.fbutton);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent1=new Intent(MainActivity.this,Main2Activity.class);
                 startActivity(intent1);
+            }
+        });
+
+        fbutton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder ad = new AlertDialog.Builder(MainActivity.this);
+                ad.setMessage("Login not yet setup try FingerPrint type Login");
+
+                ad.setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                AlertDialog ald=ad.create();
+                ald.show();
             }
         });
 
